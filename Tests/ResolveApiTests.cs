a49476d95893285bc65442f7f02876a4287fbeba
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2021-05-23 12:24:39 +0200

    Add more clear API tests

---
+++ Tests/ResolveApiTests.cs
@@ -0,0 +1,54 @@
+﻿using MatthiWare.FinancialModelingPrep;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Tests
+{
+    public class ResolveApiTests : TestingBase
+    {
+        public ResolveApiTests(ITestOutputHelper testOutput) : base(testOutput)
+        {
+        }
+
+        [Fact]
+        public void ResolveFinancialModelingPrepApi_Works()
+        {
+            var api = ServiceProvider.GetRequiredService<IFinancialModelingPrepApiClient>();
+
+            Assert.NotNull(api);
+        }
+
+        [Fact]
+        public void API_Contains_Calendars_Provider()
+        {
+            var api = ServiceProvider.GetRequiredService<IFinancialModelingPrepApiClient>();
+
+            Assert.NotNull(api.Calendars);
+        }
+
+        [Fact]
+        public void API_Contains_Company_Provider()
+        {
+            var api = ServiceProvider.GetRequiredService<IFinancialModelingPrepApiClient>();
+
+            Assert.NotNull(api.CompanyValuation);
+        }
+
+        [Fact]
+        public void API_Contains_AdvancedData_Provider()
+        {
+            var api = ServiceProvider.GetRequiredService<IFinancialModelingPrepApiClient>();
+
+            Assert.NotNull(api.AdvancedData);
+        }
+
+        [Fact]
+        public void API_Contains_MarketIndexes_Provider()
+        {
+            var api = ServiceProvider.GetRequiredService<IFinancialModelingPrepApiClient>();
+
+            Assert.NotNull(api.MarketIndexes);
+        }
+    }
+}
