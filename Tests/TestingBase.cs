a91d19e78a3805bcac44684c7360f84847e97fc0
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2021-05-23 10:17:08 +0200

    Add APIKey as env var

--- Tests/TestingBase.cs
+++ Tests/TestingBase.cs
@@ -2,6 +2,7 @@
 using MatthiWare.FinancialModelingPrep;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 using System;
 using Xunit.Abstractions;
 
@@ -12,9 +13,20 @@ namespace Tests
         protected IServiceCollection Services { get; private set; }
         protected IServiceProvider ServiceProvider { get; private set; }
 
+        private static readonly IConfigurationRoot ConfigurationRoot;
+
+        static TestingBase()
+        {
+            var config = new ConfigurationBuilder();
+            config.AddEnvironmentVariables("FMP_TESTS_");
+            ConfigurationRoot = config.Build();
+        }
+
         public TestingBase(ITestOutputHelper testOutput)
         {
             this.Services = new ServiceCollection();
+
+            this.Services.AddSingleton(ConfigurationRoot);
 
             // TODO: add logging not possible yet as I don't want to expose API Keys in the CI/CD logs
             // See:  
@@ -25,9 +37,21 @@ namespace Tests
             //    builder.SetMinimumLevel(LogLevel.Debug);
             //});
 
-            this.Services.AddFinancialModelingPrepApiClient(new FinancialModelingPrepOptions());
+            this.Services.AddFinancialModelingPrepApiClient(CreateDefaultFMPOptions());
 
             Build();
+        }
+
+        private static FinancialModelingPrepOptions CreateDefaultFMPOptions()
+        {
+            var apiKeySection = ConfigurationRoot.GetSection("API_KEY");
+
+            var apiKey = apiKeySection?.Value ?? "demo";
+
+            return new FinancialModelingPrepOptions()
+            {
+                ApiKey = apiKey
+            };
         }
 
         private static LoggingConfig CreateLoggingConfig()
