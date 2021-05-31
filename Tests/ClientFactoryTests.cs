acdaaa7167dca91cc42d9c437221cb6d4ce0e1d6
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2021-05-31 20:35:01 +0200

    Add tests for factory method

---
+++ Tests/ClientFactoryTests.cs
@@ -0,0 +1,48 @@
+﻿using MatthiWare.FinancialModelingPrep;
+using Xunit;
+
+namespace Tests
+{
+    public class ClientFactoryTests
+    {
+        [Fact]
+        public void ResolveFinancialModelingPrepApi_Works()
+        {
+            var api = FinancialModelingPrepApiClientFactory.CreateClient(new FinancialModelingPrepOptions());
+
+            Assert.NotNull(api);
+        }
+
+        [Fact]
+        public void API_Contains_Calendars_Provider()
+        {
+            var api = FinancialModelingPrepApiClientFactory.CreateClient(new FinancialModelingPrepOptions());
+
+            Assert.NotNull(api.Calendars);
+        }
+
+        [Fact]
+        public void API_Contains_Company_Provider()
+        {
+            var api = FinancialModelingPrepApiClientFactory.CreateClient(new FinancialModelingPrepOptions());
+
+            Assert.NotNull(api.CompanyValuation);
+        }
+
+        [Fact]
+        public void API_Contains_AdvancedData_Provider()
+        {
+            var api = FinancialModelingPrepApiClientFactory.CreateClient(new FinancialModelingPrepOptions());
+
+            Assert.NotNull(api.AdvancedData);
+        }
+
+        [Fact]
+        public void API_Contains_MarketIndexes_Provider()
+        {
+            var api = FinancialModelingPrepApiClientFactory.CreateClient(new FinancialModelingPrepOptions());
+
+            Assert.NotNull(api.MarketIndexes);
+        }
+    }
+}
