8a6e8510ef09cd077232d63c6bfec2d239b19250
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2021-05-31 20:02:26 +0200

    Add factory method for those who don't want to use DI

---
+++ FinancialModelingPrepApi/FinancialModelingPrepApiClientFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MatthiWare.FinancialModelingPrep
+{
+    /// <summary>
+    /// Exposes factory methods to easily create clients without using dependency injection.
+    ///  If you do want to use DI please see <see cref="DependencyInjectionExtensions.AddFinancialModelingPrepApiClient(IServiceCollection, FinancialModelingPrepOptions)"/>
+    /// </summary>
+    public static class FinancialModelingPrepApiClientFactory
+    {
+        /// <summary>
+        /// Creates an FMP API Client with all dependencies already wired up.
+        /// If you do want to use DI please see <see cref="DependencyInjectionExtensions.AddFinancialModelingPrepApiClient(IServiceCollection, FinancialModelingPrepOptions)"/>
+        /// </summary>
+        /// <param name="options"><see cref="FinancialModelingPrepOptions"/></param>
+        /// <returns>An instance of <see cref="IFinancialModelingPrepApiClient"/></returns>
+        public static IFinancialModelingPrepApiClient CreateClient(FinancialModelingPrepOptions options)
+        {
+            var services = new ServiceCollection();
+
+            services.AddFinancialModelingPrepApiClient(options);
+
+            var provider = services.BuildServiceProvider();
+
+            return provider.GetRequiredService<IFinancialModelingPrepApiClient>();
+        }
+    }
+}
