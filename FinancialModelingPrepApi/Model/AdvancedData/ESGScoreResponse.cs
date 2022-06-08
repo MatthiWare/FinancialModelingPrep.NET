593407cb74f95c7223bc27c94c367a9008e9878a
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2022-06-08 19:23:11 +0200

    Add ESG Score

---
+++ FinancialModelingPrepApi/Model/AdvancedData/ESGScoreResponse.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MatthiWare.FinancialModelingPrep.Model.AdvancedData
+{
+    public class ESGScoreResponse
+    {
+        [JsonPropertyName("symbol")]
+        public string Symbol { get; set; }
+
+        [JsonPropertyName("cik")]
+        public string Cik { get; set; }
+
+        [JsonPropertyName("companyName")]
+        public string CompanyName { get; set; }
+
+        [JsonPropertyName("formType")]
+        public string FormType { get; set; }
+
+        [JsonPropertyName("acceptedDate")]
+        public string AcceptedDate { get; set; }
+
+        [JsonPropertyName("date")]
+        public string Date { get; set; }
+
+        [JsonPropertyName("environmentalScore")]
+        public double EnvironmentalScore { get; set; }
+
+        [JsonPropertyName("socialScore")]
+        public double SocialScore { get; set; }
+
+        [JsonPropertyName("governanceScore")]
+        public double GovernanceScore { get; set; }
+
+        [JsonPropertyName("ESGScore")]
+        public double ESGScore { get; set; }
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+    }
+}
