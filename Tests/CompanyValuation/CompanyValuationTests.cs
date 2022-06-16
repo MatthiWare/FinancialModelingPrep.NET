24ea29a0fb9d6eb1fffb98d13eec2b4a844cab6d
Walter Franssen <w.franssen@gmail.com>
2022-06-16 18:30:31 +0200

    Add nullalbe exchange on company search search (#35)
    * Add nullalbe exchange on company search search
    
    * fixed unit test
    
    Co-authored-by: Walter Franssen <walter.franssen@centric.eu>

--- Tests/CompanyValuation/CompanyValuationTests.cs
+++ Tests/CompanyValuation/CompanyValuationTests.cs
@@ -364,6 +364,19 @@ namespace Tests.CompanyValuation
         }
 
         [Fact]
+        public async Task SearchByTickerWithoutExchangeAsync()
+        {
+            var result = await api.SearchByTickerAsync("AAPL", null, 5);
+
+            result.AssertNoErrors();
+            Assert.NotEmpty(result.Data);
+            Assert.True(result.Data.Count >= 1);
+
+            var firstResult = result.Data.First(_ => _.Symbol == "AAPL");
+            Assert.Equal("AAPL", firstResult.Symbol);
+        }
+
+        [Fact]
         public async Task GetHistoricalMarketCapAsync()
         {
             var result = await api.GetHistoricalMarketCapitalizationAsync("AAPL", 5);
